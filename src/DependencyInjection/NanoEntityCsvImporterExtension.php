eb692fd293733d0c36545843543fb813339f1478
lasse-nano <111313096+lasse-nano@users.noreply.github.com>
2023-11-16 21:29:22 +0100

    add Extension

--- src/DependencyInjection/NanoEntityCsvImporterExtension.php
+++ src/DependencyInjection/NanoEntityCsvImporterExtension.php
@@ -6,7 +6,7 @@ use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
-class NanoEntityCsvImporterExtension
+class NanoEntityCsvImporterExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container): void
     {
